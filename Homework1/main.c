a6b4469f7dc1e3ae55775d9ef94b72dbaa3c928e
Ben Mizera <mizerb@rpi.edu>
2016-01-28 19:13:58 -0500

    intial

---
+++ Homework1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+//WOOT WOOT
+
+//read in file char at a time? Easier parsing? Or fscanf()? 
+
+int main(int argv , char* argc[]){
+  printf("Starting");
+  
+  if(argv != 3){
+    printf("USAGE ARG 1: FILENAME \n ARG2: SEARCH STRING");
+  }
+
+  
