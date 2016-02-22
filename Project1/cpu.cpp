0e731079b95d34d5dc87e3825ed0fd4250da9bbc
Benjamin Mizera <mizerb@rpi.edu>
2016-02-22 15:01:31 -0500

    Refactored core

--- Project1/cpu.cpp
+++ Project1/cpu.cpp
@@ -48,48 +48,6 @@ void Result::write_out(FILE* of , std::string name)
   return;
 }
 
-void Core::increment()
-{
-  if(is_context_swapping)
-  {
-  	context_countdown--;
-  	burst_now.is_context_swap();
-  } 
-  else burst_now.in_cpu_incre();
-}
-
-void Core::receive_proc( Proc new_proc)
-{
-  burst_now = new_proc;
-  is_context_swapping = false;
-  has_proc = true;
-}
-
-bool Core::rdy_for_proc()
-{
-  return (!has_proc && !is_context_swapping);
-}
-
-bool Core::rdy_to_start()
-{
-  return (is_context_swapping && (context_countdown==0));
-}
-
-void Core::start_context_swap()
-{
-  is_context_swapping = true;
-  context_countdown = t_cs;
-  has_proc = false;
-  return;
-}
-
-void Core::wait_for_proc()
-{
-  is_context_swapping = false;
-  has_proc = false;
-}
-
-
 Cpu::Cpu()
 {
   time =0;
@@ -191,7 +149,7 @@ void Cpu::increment_cores()
   return;
 }
 
-/*
+
 void Cpu::cores_check_all()
 {
   for(std::list<Core>::iterator it= cores.begin(); it != cores.end() ; ++it )
@@ -213,8 +171,8 @@ void Cpu::cores_check_all()
     }
   }
 }
-*/
 
+/*
 void Cpu::cores_check_all()
 {
   for(std::list<Core>::iterator it= cores.begin(); it != cores.end() ; ++it )
@@ -242,7 +200,7 @@ void Cpu::cores_check_all()
     }
   }
 }
-
+*/
 
 
 
