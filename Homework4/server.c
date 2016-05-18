16c8d7dd87d39114cd9829e7eb0509730e308b9a
Benjamin Mizera <mizerb@rpi.edu>
2016-05-18 19:53:52 -0400

    the moving forward

--- Homework4/server.c
+++ Homework4/server.c
@@ -1,3 +1,7 @@
+/* BEN MIZERA
+   He's a jolly good fello.
+*/
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/file.h>
@@ -69,11 +73,11 @@ int error_lookup(int error_num , char * error_discript)
 
 
 
-int write_from_socket( FILE * socket, char * filename, int size)
+int write_from_socket( int newsock , char * filename, int size)
 {
     // CHECK FOR FILENAME already existing
 
-    if( access( filename, F_OK | R_OK | W_OK ) == -1)
+    if( access( filename, F_OK ) == 0)
     {
         // FILE EXISTS
         return FILE_EXISTS;
@@ -87,7 +91,7 @@ int write_from_socket( FILE * socket, char * filename, int size)
 
     //check for calloc sucess
 
-    int n = (int)fread( file_content , sizeof(char), size ,socket);
+    int n = recv(newsock , file_content , size , 0);
 
     if( n != size)
     {
@@ -125,20 +129,11 @@ int write_from_socket( FILE * socket, char * filename, int size)
     n = (int) fwrite( file_content, sizeof(char) , size , outFile);
     if( n != size)
     {
-        if( n < 0 )
-        {
-            // real error
-            //Todo: Find the errors, or report error?
-            free (file_content);
-        }
-        else
-        {
-            //not all bytes written
-            free (file_content);
-            //flock(outFile, LOCK_UN);
-            fclose( outFile);
-            return FILE_ERROR_WHILE_WRITING;
-        }
+        //not all bytes written
+        free (file_content);
+        //flock(outFile, LOCK_UN);
+        fclose( outFile);
+        return FILE_ERROR_WHILE_WRITING;
     }
 
     //Remove file lock 
@@ -151,9 +146,11 @@ int write_from_socket( FILE * socket, char * filename, int size)
     return 0;
 }
 
-int read_to_socket( FILE * socket, char * filename , int start, int length)
+int read_to_socket( int newsock, char * filename , int start, int length , char ** ret_prt)
 {
-    if( access( filename, F_OK | R_OK  ) != -1)
+    //printf( " START : %d , SIZE : %d \n" , start, length);
+    char * ret_msg;
+    if( access( filename, F_OK | R_OK  ) != 0)
     {
         //FILE DOES NOT EXIST, THROW A TANTURNM
         return FILE_NOT_FOUND;
@@ -175,36 +172,42 @@ int read_to_socket( FILE * socket, char * filename , int start, int length)
         return INVALID_BYTE_RANGE;
     }
 
-    char * content = calloc( length , sizeof(char));
+    ret_msg = calloc( length + 1  , sizeof(char));
 
     fseek(read_file, (long)start , SEEK_SET);
 
-    size = (int)fread( content, sizeof(char) ,length , read_file);
+    size = (int)fread( ret_msg, sizeof(char) ,length , read_file);
 
     if( size != length)
     {
         if( size > 0 )
         {
-            free(content);
+            free(ret_msg);
             fclose(read_file);
             return SIZE_ERROR_READING;
         }
         else 
         {
             // real error
-            free (content);
+            free (ret_msg);
             fclose (read_file);
             return 11;
         }
         
     }
+    ret_msg[length] = '\0';
+    
+    //printf("%p\n",ret_msg );
+    
 
+    ret_prt[0] = ret_msg;
     return 0;
 }
 
-int delete_file( FILE * socket , char * filename )
+int delete_file( int newsock , char * filename )
 {
-    if( access( filename, F_OK | R_OK  ) != -1)
+    
+    if( access( filename, F_OK | R_OK  ) != 0)
     {
         //FILE DOES NOT EXIST, THROW A TANTURNM
         return FILE_NOT_FOUND;
@@ -224,13 +227,18 @@ struct cmd_content
     int command;
     int size;
     int start;
+    int more;
 };
 
 struct cmd_content get_command(int newsock , char start)
 {
-    struct cmd_content Ret; char * cmd_string; char test[1024]; 
+    struct cmd_content Ret; char test[1024]; 
     char BUFFER[1024]; char* a = BUFFER+1; int n = 2;
     BUFFER[0] = start;
+    //int cmd_end;
+    Ret.filename = malloc(128 * sizeof(char));
+    Ret.command = -1; Ret.start =-1 ; Ret.size = -1;
+    
     do
     {
         int s = recv( newsock, a , 1, 0 );
@@ -241,51 +249,67 @@ struct cmd_content get_command(int newsock , char start)
             exit(1);
         }
 
-    }while( a != '\n');
+    }while( *(a++) != '\n');
 
     BUFFER[n] = '\0';
-
-    //sscanf(BUFFER,"%s", cmd_string );
+    //printf("%s\n",BUFFER );
 
     if( strstr(BUFFER, "DELETE") == BUFFER )
     {
         sscanf(BUFFER, "DELETE %s\n", test);
-        Ret.command = DEL_COM;
-        sprintf(cmd_string, "DELETE %s", Ret.filename);
+        if( strlen(test) > 0)
+        {
+            strncpy( Ret.filename, test , strlen(test));
+            Ret.command = DEL_COM;
+        }
+        //sprintf(cmd_string, "DELETE %s", Ret.filename);
     }
     else if( strstr(BUFFER, "STORE") == BUFFER)
     {
-        sscanf(BUFFER , "STORE %d\n" , test)
-        Ret.command = STO_COM;
-        strcpy( Ret.filename, test ,strlen(test));
+        //printf("I hate life\n");
+        sscanf(BUFFER , "STORE %s %d\n" , test , &Ret.size);
         
-        sprintf(cmd_string, "STORE %s %d", Ret.filename , Ret.size);
+        if( Ret.size > 0 && strlen(test) > 0)
+        {
+            Ret.command = STO_COM;
+            strncpy( Ret.filename, test , strlen(test)); 
+        }
+        //sprintf(cmd_string, "STORE %s %d", Ret.filename , Ret.size);
+
+        //printf("I hate life\n");
     }
     else if (strstr(BUFFER, "READ") == BUFFER)
     {
-        sscanf(BUFFER, "READ %s %d %d\n", test , Ret.size , Ret.start);
-        Ret.command = RDD_COM;
-        strcpy( Ret.filename, test ,strlen(test));
+        sscanf(BUFFER, "READ %s %d %d\n", test , &Ret.start, &Ret.size);
+        
+        if( Ret.start >= 0 && Ret.size > 0 && strlen(test) > 0)
+        {
+            strncpy( Ret.filename, test ,strlen(test));
 
-        sprintf(cmd_string, "READ %s %d %d", Ret.filename ,Ret.start, Ret.size);
+            //sprintf(cmd_string, "READ %s %d %d", Ret.filename , Ret.start, Ret.size );
+            Ret.command = RDD_COM;
+        }
+        //printf( " START : %d , SIZE : %d \n" , Ret.start, Ret.size);
     }
-    else
+    if( Ret.command == -1)
     {
         //COMMAND UNKOWN
         // CANNOT PARSE THIS SHIT
         Ret.command = -1;
-        sprintf(BUFFER,"%s", cmd_string);
+        printf("[child %d] Rcvd: %s\n", getpid(),BUFFER);
+        return Ret;
+        //sprintf(BUFFER,"%s\n", cmd_string);
     }
 
-    printf("[child %d] Rcvd: %s\n", getpid(),cmd_string);
+    printf("[child %d] Rcvd: %s", getpid(),BUFFER);
     //printf("[child %d] Rcvd: %s\n", getpid(), BUFFER);
 
     return Ret;
 }
 
-int send_ACK( int newsock , int data_size)
+void send_ACK( int newsock , int data_size)
 {
-    int n , size; char * msg, * extra;
+    int n , size; char msg[128], extra[128]; int x =0;
     if( data_size == -1)
     {
         sprintf(msg,"ACK\n"); size = 4;
@@ -295,17 +319,18 @@ int send_ACK( int newsock , int data_size)
         sprintf(extra , "%d\n" , data_size);
         size = strlen(extra)+4;
         sprintf(msg, "ACK %s", extra);
+        x = MSG_MORE;
     }
 
-    n = send( newsock, msg, size, 0 );
+    n = send( newsock, msg, size, x );
     fflush( NULL );
     if ( n != size)
       perror( "send() failed\n" );
     
-    printf("[child %d] Sent: %s\n", getpid(), msg);
+    printf("[child %d] Sent: %s", getpid(), msg);
 }
 
-int send_Error(int newsock , char * error_msg)
+void send_Error(int newsock , char * error_msg)
 {
     int n, size;
     size = strlen(error_msg);
@@ -315,46 +340,82 @@ int send_Error(int newsock , char * error_msg)
     if ( n != size)
       perror( "[child %d] send() failed\n" );
     else 
-        printf("[child %d] Sent: %s\n", getpid(), error_msg);
+        printf("[child %d] Sent: %s", getpid(), error_msg);
 }
 
 void Read( int newsock , struct cmd_content cmd)
 {
-    int err, start, size;
-    FILE * temp;
+    int err, n; char * msg = NULL; char BUFFER[128];
     
-    err = read_to_socket(temp , cmd.filename, start, size);
+    
+    err = read_to_socket(newsock , cmd.filename, cmd.start, cmd.size, &msg);
+    
+    //printf("MSG%p\n", msg);
+
+    //printf( "%s\n", msg);
+    
     if( err != 0 )
     {
-        char * error_msg;
+        char error_msg[128];
         error_lookup(err , error_msg);
-        send_Error( newsock, error_msg);        
+        send_Error( newsock, error_msg);
+        //free(complete_msg);
+        free(msg);        
+        return;
     }
-    else
-    {
-        printf("[child %d] Deleted '%s' from server\n", 
-            getpid(), cmd.filename);
+    
+    //printf("SIZE: %d\n" , cmd.size);
 
-        send_ACK(newsock, size);
+    //send_ACK(newsock, cmd.size);
+    //printf("herte\n");
+    
+    sprintf(BUFFER, "ACK %d\n", cmd.size);
+    
+    //printf("here%d\n", strlen(msg));
+    
+
+    //The Hw is ambigous wether or not to use 2 sends, soooo, I use 1. 
+    char *complete_msg = malloc( (strlen(msg) + strlen(BUFFER) )*sizeof(char));
+    
+    // printf("here\n" );
+    strcat(complete_msg, BUFFER);
+    strcat(complete_msg, msg);
+    //printf("%s\n", complete_msg);
+    
+    int z = strlen(complete_msg);
+
+    n = send( newsock, msg, z , 0 );
+    free(complete_msg);
+    free(msg);
+    if( n != z)
+    {
+        //printf("DUCKING DUCK FACE DUCKING DUKE %d  %d\n" , n, strlen(complete_msg));
+        printf("[child %d] ERROR: Send Failed\n" , getpid());
+        
+        return;
     }
+    printf("[child %d] Sent: %s", getpid(), BUFFER);
+    printf("[child %d] Sent %d bytes of '%s' from offset %d\n", 
+        getpid(), (int)(z-strlen(BUFFER)) , cmd.filename, cmd.start);
+
+    
 }
 
 void Write( int newsock ,struct cmd_content cmd)
 {
-    int err, size;
-    FILE * temp;
+    int err;
 
-    err = write_from_socket( temp, cmd.filename , size);
+    err = write_from_socket( newsock, cmd.filename , cmd.size);
     if( err != 0 )
     {
-        char * error_msg;
+        char  error_msg[128];
         error_lookup(err , error_msg);
         send_Error( newsock, error_msg);        
     }
     else
     {
         printf("[child %d] Stored file '%s' (%d bytes)\n", getpid() ,
-         cmd.filename, size);
+         cmd.filename, cmd.size);
 
         send_ACK(newsock, -1);
     }
@@ -362,13 +423,13 @@ void Write( int newsock ,struct cmd_content cmd)
 
 void Delete( int newsock , struct cmd_content cmd)
 {
-    int err; FILE * temp;
+    int err;
 
-    err = delete_file(temp, cmd.filename );
+    err = delete_file(newsock, cmd.filename );
 
     if( err != 0 )
     {
-        char * error_msg;
+        char error_msg[128];
         error_lookup(err , error_msg);
         send_Error( newsock, error_msg);        
     }
@@ -385,7 +446,7 @@ void Delete( int newsock , struct cmd_content cmd)
 void Run_Connection(int newsock)
 {
     //Stepping on the beach 
-    char BUFFER[256]; int n; char start;
+    int n; char start;
     do
     {
         n = recv(newsock , &start , 1 , 0);
@@ -408,8 +469,13 @@ void Run_Connection(int newsock)
             //command not rec
             send_Error(newsock, "Error: Command not usable\n");
         }
+        free(cmd.filename);
         
     }while( n > 0);
+
+    printf("[child %d] Client closed its socket....terminating\n" , getpid());
+    close(newsock);
+    return;
 }
 
 
@@ -417,7 +483,7 @@ int main(int argc, char  *argv[])
 {
     /* I don't know */
     
-    int sockfd, newsockfd, portno, clilen , pid , n;
+    int sockfd, pid ;
     struct sockaddr_in serv_addr, cli_addr;
     
     sockfd = socket( PF_INET, SOCK_STREAM, 0 );
@@ -443,14 +509,14 @@ int main(int argc, char  *argv[])
 
     listen( sockfd , 5);
 
-    clilen = sizeof(cli_addr);
+    socklen_t clilen = sizeof(cli_addr);
 
     printf("Listening on port %d\n" , port);
 
     while(1)
     {
         int newsock = accept(sockfd, (struct sockaddr *)&cli_addr,
-                              &clilen);
+                              &(clilen));
 
         printf("Received incoming connection from %s\n", 
             inet_ntoa( (struct in_addr)cli_addr.sin_addr));
@@ -463,7 +529,7 @@ int main(int argc, char  *argv[])
             pid = send( newsock, "ERROR: FORK FAILED\n", 19, 0 );
             return EXIT_FAILURE;
         }
-        if( pid ==0 )
+        if( pid == 0 )
         {
             Run_Connection( newsock);
         }
